89a20c4d4e8e8472782f2c1ce51f596654047928
LeahESc <leahemily1@gmail.com>
2020-11-30 08:47:33 -0800

    adds /logout route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
+  resources :properties
+  resources :locations
   devise_for :users, :controllers => {registrations: 'registrations', omniauth_callbacks: 'callbacks'}
-  root to: "application#welcome"
-
   devise_scope :user do 
     get '/login', to: 'devise/sessions#new', as: 'login'
     get '/signup', to: 'devise/registrations#new', as: 'signup'
+    get '/logout', to: 'devise/sessions#destroy', as: 'logout'
   end 
+  
+  root to: "application#welcome"
 end
