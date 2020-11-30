d6d61fe4eb0d104648c067e176a6573e887275fa
LeahESc <leahemily1@gmail.com>
2020-11-30 12:44:17 -0800

    formats new form with bootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,9 @@ class ApplicationController < ActionController::Base
     
    
     def welcome
-
+      if user_signed_in? 
+        redirect_to properties_path
+      end
     end 
 
     # protected
