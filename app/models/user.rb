d9cdc4dd2890d538f6ad0e10c9edc71cbbd942f2
LeahESc <leahemily1@gmail.com>
2020-11-30 10:50:44 -0800

    adds google auth capability to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable, 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, authentication_keys: [:login]
+         :recoverable, :rememberable, :validatable, 
+         :omniauthable, omniauth_providers: [:github, :google_oauth2], authentication_keys: [:login]
   has_many :properties
   has_many :locations, through: :properties
   validates :username, presence: true, uniqueness: { case_sensitive: false }
@@ -21,15 +22,23 @@ class User < ApplicationRecord
     end
   end
 
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.provider = auth.provider
-      user.username = auth.info.name
-      user.uid = auth.uid
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0..20]
-    end 
-  end 
+  def self.create_from_provider_data(provider_data)
+    where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do |user|
+    user.username = provider_data.info.name
+    user.email = provider_data.info.email
+    user.password = Devise.friendly_token[0,20]
+    end
+  end
+
+  # def self.from_omniauth(auth)
+  #   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+  #     user.provider = auth.provider
+  #     user.username = auth.info.name
+  #     user.uid = auth.uid
+  #     user.email = auth.info.email
+  #     user.password = Devise.friendly_token[0..20]
+  #   end 
+  # end 
 
 
 end
