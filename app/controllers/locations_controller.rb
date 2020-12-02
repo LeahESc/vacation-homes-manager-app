e48ed8cfbe93a2efcd1e71b60e6416eb3ca1c782
LeahESc <leahemily1@gmail.com>
2020-12-02 10:31:30 -0800

    adds image formatting

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,23 +1,16 @@
 class LocationsController < ApplicationController
-    
-    def index 
-        if user_signed_in? 
-            @locations = current_user.locations
-        else  
-        redirect_to '/'
-        end 
+    before_action :redirect
+
+    def index  
+            @locations = current_user.locations.uniq
     end 
 
-    def show 
-        if user_signed_in? 
-            @location = current_user.locations.find_by(id: params[:id])
-            if @location 
-                @properties = current_user.properties.where(location_id: params[:id])
-                render :show 
-            end   
-        else  
-        redirect_to '/' 
-        end  
+    def show  
+        @location = current_user.locations.find_by(id: params[:id])
+        if @location 
+            @properties = current_user.properties.where(location_id: params[:id])
+            render :show 
+        end 
     end 
 
 
